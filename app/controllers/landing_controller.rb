262871e16fa48bda4518847acb99d6f0bd409a30
mattcheang <cheangmatt@gmail.com>
2017-11-04 22:13:05 -0700

    add pages

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,16 @@
+class LandingController < ApplicationController
+  def home
+  end
+
+  def news
+  end
+
+  def content
+  end
+
+  def products
+  end
+
+  def goals
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'news' => 'landing#news'
+  get 'content' => 'landing#content'
+  get 'products' => 'landing#products'
+  get 'goals' => 'landing#goals'
+  root 'landing#home'
 end
